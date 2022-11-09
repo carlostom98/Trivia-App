313fe480aedd596e77d21ebd2a295c0200bc7d5f
Carlos Andrés Martínez <carams500@hotmail.com>
2022-11-09 17:26:53 -0500

    First way without navigation, now we will implement navigation components

--- app/src/main/java/com/example/navigation/MainActivity.kt
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -9,23 +9,13 @@ import com.example.navigation.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var drawerLayout:DrawerLayout
-    private lateinit var actionBarDrawerLayout:ActionBarDrawerToggle
-    private lateinit var toolbar:androidx.appcompat.widget.Toolbar
-    private lateinit var navigationView:NavigationView
 
-    private lateinit var binding:ActivityMainBinding
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        toolbar=findViewById(R.id.tool_bar)
-        setSupportActionBar(toolbar)
-        drawerLayout=findViewById(R.id.main_layout)
-        navigationView=findViewById(R.id.navigationView)
-        actionBarDrawerLayout= ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close)
-        drawerLayout.addDrawerListener(actionBarDrawerLayout)
-        actionBarDrawerLayout.isDrawerIndicatorEnabled
-        actionBarDrawerLayout.syncState()
+
     }
 }
