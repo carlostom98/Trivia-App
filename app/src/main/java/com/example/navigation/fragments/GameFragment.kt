f83011c0c8ed42c2b7d46b6c2031120454decdbd
Carlos Andrés Martínez <carams500@hotmail.com>
2022-11-10 09:30:20 -0500

    Backstack manipulation done

--- app/src/main/java/com/example/navigation/fragments/GameFragment.kt
+++ app/src/main/java/com/example/navigation/fragments/GameFragment.kt
@@ -5,30 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.example.navigation.MainActivity
 import com.example.navigation.R
-import com.example.navigation.databinding.FragmentHomeBinding
-import kotlin.random.Random
+import com.example.navigation.databinding.FragmentGameBinding
 
-class HomeFragment : Fragment() {
+class GameFragment : Fragment() {
 
-    private var _binding:FragmentHomeBinding?=null
+    private var _binding:FragmentGameBinding?=null
     private val binding get() = _binding!!
-    private lateinit var activity:MainActivity
 
+    private lateinit var activity:MainActivity
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         this.activity = getActivity() as MainActivity
-
-        _binding=FragmentHomeBinding.inflate(inflater, container, false)
-
-        binding.buttonPlay.setOnClickListener {
-           it.findNavController().navigate(R.id.action_homeFragment_to_gameFragment)
+        _binding= FragmentGameBinding.inflate(inflater,container,false)
+        binding.continueButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_gameFragment_to_phoneFragment)
         }
         return binding.root
     }
